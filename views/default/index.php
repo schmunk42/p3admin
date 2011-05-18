64285f0c956b836b9e5912d4507e96aa5327e736
Tobias Munk <schmunk@usrbin.de>
2011-05-18 12:08:57 +0200

    fixed login, when using the rights module - DO NOT use the standard login page

--- views/default/index.php
+++ views/default/index.php
@@ -1,5 +1,5 @@
 <?php
-$this->breadcrumbs=array(
+$this->breadcrumbs = array(
 	$this->module->id,
 );
 ?>
@@ -12,23 +12,88 @@ $this->breadcrumbs=array(
 </p>
 
 <table class="span-12 append-12 last">
-<?php foreach($this->getModuleData() AS $name => $config): ?>
-	<tr>
-		<td>
-			<?php echo CHtml::link($name,array('/'.$name)) ?>
+	<?php foreach ($this->getModuleData() AS $name => $config): ?>
+		<tr>
+			<td>
+			<?php echo CHtml::link($name, array('/' . $name)) ?>
 		</td>
 		<td>
-			<?php echo ($config !== null)?$config['class']:'<em>Not configured yet</em>' ?>
+			<?php echo ($config !== null) ? $config['class'] : '<em>Not configured yet</em>' ?>
 		</td>
 		<td>
-			<?php echo CHtml::link('Manage', array('/p3admin/module', 'module'=>$name)) ?>
+			<?php echo CHtml::link('Manage', array('/p3admin/module', 'module' => $name)) ?>
 		</td>
 	</tr>
-<?php endforeach; ?>
-</table>
+	<?php endforeach; ?>
+		</table>
+		<hr/>
+		<div class="span-24 last">
+			<h2>Current User</h2>
+			<h3>Information</h3>
+			<div class="span-4">
+				<h4>Name</h4>
+				<p><?php echo Yii::app()->user->name ?></p>
+			</div>
+			<div class="span-4">
+				<h4>Id</h4>
+				<p><?php echo Yii::app()->user->id ?></p>
+			</div>
+		</div>
 
-<div class="span-18 last">
-    <h2>Current User</h2>
-    <h3>Name</h3>
-    <p><?php echo Yii::app()->user->name ?></p>
+
+		<div class="span-24">
+			<h3>Assigned Auth Items</h3>
+
+			<div class="span-8">
+				<h4>Roles</h4>
+				<p>
+			<?php foreach (Yii::app()->authManager->getAuthItems(2, Yii::app()->user->id) AS $key => $value)
+				echo $key . ", " ?>
+			</p>
+		</div>
+		<div class="span-8">
+			<h4>Operations</h4>
+			<p><?php
+				foreach (Yii::app()->authManager->getAuthItems(1, Yii::app()->user->id) AS $key => $value)
+					echo $key . ", "
+			?></p>
+			</div>
+			<div class="span-8 last">
+				<h4>Tasks</h4>
+				<p><?php
+					foreach (Yii::app()->authManager->getAuthItems(0, Yii::app()->user->id) AS $key => $value)
+						echo $key . ", "
+			?></p>
+				</div>
+
+
+			</div>
+
+			<hr/>
+
+			<div class="span-18 last">
+				<h2>Configuration</h2>
+
+				<h3>
+																				Parameters
+				</h3>
+				<p>
+<?php
+						echo CVarDumper::dumpAsString(Yii::app()->params, 10, true);
+?></p>
+					<h3>
+																									Modules
+					</h3>
+					<p>
+<?php
+						echo CVarDumper::dumpAsString(Yii::app()->getModules(), 10, true);
+?></p>
+					<h3>
+																									Components
+					</h3>
+					<p>
+<?php
+						echo CVarDumper::dumpAsString(Yii::app()->getComponents(), 10, true);
+?></p>
+
 </div>
