33857d99eccd41b8c019be22aa85faa626a922c5
Tobias Munk <schmunk@usrbin.de>
2012-10-04 21:15:07 +0200

    removed login view and action, as it's handled by vendor module

--- commands/views/p3-webapp/controllers/SiteController.php
+++ commands/views/p3-webapp/controllers/SiteController.php
@@ -73,30 +73,8 @@ class SiteController extends Controller
 	}
 
 	/**
-	 * Displays the login page
+	 * Login page handled via vendor module mishamx.yii-user
 	 */
-	public function actionLogin()
-	{
-		$model=new LoginForm;
-
-		// if it is ajax validation request
-		if(isset($_POST['ajax']) && $_POST['ajax']==='login-form')
-		{
-			echo CActiveForm::validate($model);
-			Yii::app()->end();
-		}
-
-		// collect user input data
-		if(isset($_POST['LoginForm']))
-		{
-			$model->attributes=$_POST['LoginForm'];
-			// validate user input and redirect to the previous page if valid
-			if($model->validate() && $model->login())
-				$this->redirect(Yii::app()->user->returnUrl);
-		}
-		// display the login form
-		$this->render('login',array('model'=>$model));
-	}
 
 	/**
 	 * Logs out the current user and redirect to homepage.
