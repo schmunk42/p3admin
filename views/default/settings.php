46e09006e3e4991032b4a978134498f46d189f48
Tobias Munk <schmunk@usrbin.de>
2013-05-20 23:07:19 +0200

    added missing "endforeach;"; code formatting

--- views/default/settings.php
+++ views/default/settings.php
@@ -4,23 +4,23 @@ $this->breadcrumbs = array(
 );
 ?>
 
-<?php $this->widget("TbBreadcrumbs", array("links"=>$this->breadcrumbs)) ?>
+<?php $this->widget("TbBreadcrumbs", array("links" => $this->breadcrumbs)) ?>
 
 <h1>
-	<?php echo Yii::t('P3AdminModule.crud', 'Application'); ?>
-	<small><?php echo Yii::t('P3AdminModule.crud', 'Settings'); ?></small>
+    <?php echo Yii::t('P3AdminModule.crud', 'Application'); ?>
+    <small><?php echo Yii::t('P3AdminModule.crud', 'Settings'); ?></small>
 </h1>
 
 <?php $this->beginClip('modules') ?>
 <ul class="thumbnails">
     <?php foreach ($this->getModuleData() AS $name => $config): ?>
-    <li class="span4">
-        <div class="thumbnail">
-            <h3><?php echo CHtml::link($name, array('/' . $name)) ?></h3>
-            <small><?php echo ($config !== null) ? str_replace(".", "<wbr>.", $config['class']) :
-                '<em>Not configured yet</em>' ?></small>
-        </div>
-    </li>
+        <li class="span4">
+            <div class="thumbnail">
+                <h3><?php echo CHtml::link($name, array('/' . $name)) ?></h3>
+                <small><?php echo ($config !== null) ? str_replace(".", "<wbr>.", $config['class']) :
+                        '<em>Not configured yet</em>' ?></small>
+            </div>
+        </li>
     <?php endforeach; ?>
     </table>
 </ul>
@@ -31,17 +31,18 @@ $this->breadcrumbs = array(
 <h3>App</h3>
 <ul>
     <?php foreach ($this->module->findApplicationControllers() AS $name): ?>
-    <li><?php echo CHtml::link($name, array('/' . $name)) ?></li>
+        <li><?php echo CHtml::link($name, array('/' . $name)) ?></li>
     <?php endforeach; ?>
 </ul>
 
 <?php foreach ($this->getModuleData() AS $name => $config): ?>
-<h3><?php echo CHtml::link($name, array('/' . $name)) ?></h3>
-<ul>
-    <?php foreach ($this->module->findApplicationControllers($config['class']) AS $controller_name): ?>
-    <li><?php echo CHtml::link($controller_name, array('/' . $name.'/'. $controller_name,)) ?></li>
-    <?php endforeach; ?>        
-</ul>    
+    <h3><?php echo CHtml::link($name, array('/' . $name)) ?></h3>
+    <ul>
+        <?php foreach ($this->module->findApplicationControllers($config['class']) AS $controller_name): ?>
+            <li><?php echo CHtml::link($controller_name, array('/' . $name . '/' . $controller_name,)) ?></li>
+        <?php endforeach; ?>
+    </ul>
+<?php endforeach; ?>
 <?php $this->endClip() ?>
 
 
@@ -58,8 +59,11 @@ disabled
 <ul>
     <?php
     $json = CJSON::decode(file_get_contents(Yii::getPathOfAlias('root') . DIRECTORY_SEPARATOR . 'composer.lock'));
-    if (isset($json['packages'])) foreach ($json['packages'] AS $package) {
-        echo "<li><span class=''>" . CHtml::link($package['name'], (isset($package['homepage']))?$package['homepage']:'') . "</span> <span class='label'>" . $package['version'] . "</span></li>";
+    if (isset($json['packages'])) {
+        foreach ($json['packages'] AS $package) {
+            echo "<li><span class=''>" . CHtml::link($package['name'], (isset($package['homepage'])) ?
+                    $package['homepage'] : '') . "</span> <span class='label'>" . $package['version'] . "</span></li>";
+        }
     };
     ?>
 </ul>
@@ -67,8 +71,11 @@ disabled
 <ul>
     <?php
     $json = CJSON::decode(file_get_contents(Yii::getPathOfAlias('root') . DIRECTORY_SEPARATOR . 'composer.lock'));
-    if (isset($json['packages-dev'])) foreach ($json['packages-dev'] AS $package) {
-        echo "<li><span class=''>" . CHtml::link($package['name'], (isset($package['homepage']))?$package['homepage']:'') . "</span> <span class='label'>" . $package['version'] . "</span></li>";
+    if (isset($json['packages-dev'])) {
+        foreach ($json['packages-dev'] AS $package) {
+            echo "<li><span class=''>" . CHtml::link($package['name'], (isset($package['homepage'])) ?
+                    $package['homepage'] : '') . "</span> <span class='label'>" . $package['version'] . "</span></li>";
+        }
     };
     ?>
 </ul>
@@ -87,25 +94,25 @@ disabled
 <?php
 $this->widget('bootstrap.widgets.TbTabs',
               array(
-                   'type' => 'tabs',
+                   'type'      => 'tabs',
                    'placement' => 'above', // 'above', 'right', 'below' or 'left'
-                   'tabs' => array(
-                       array('label' => Yii::t('P3AdminModule.crud', 'Modules'),
+                   'tabs'      => array(
+                       array('label'   => Yii::t('P3AdminModule.crud', 'Modules'),
                              'content' => $this->clips['modules'],
-                             'active' => true),
-                       array('label' => Yii::t('P3AdminModule.crud', 'Controllers'),
+                             'active'  => true),
+                       array('label'   => Yii::t('P3AdminModule.crud', 'Controllers'),
                              'content' => $this->clips['controllers']),
-                       array('label' => Yii::t('P3AdminModule.crud', 'Packages'),
+                       array('label'   => Yii::t('P3AdminModule.crud', 'Packages'),
                              'content' => $this->clips['__pkg']),
-                       array('label' => Yii::t('P3AdminModule.crud', 'User'),
+                       array('label'   => Yii::t('P3AdminModule.crud', 'User'),
                              'content' => $this->renderPartial('_user', array(), true)),
-                       array('label' => Yii::t('P3AdminModule.crud', 'Configuration'),
+                       array('label'   => Yii::t('P3AdminModule.crud', 'Configuration'),
                              'content' => $this->renderPartial('_config', array(), true),
                        ),
-                       array('label' => Yii::t('P3AdminModule.crud', 'Settings'),
+                       array('label'   => Yii::t('P3AdminModule.crud', 'Settings'),
                              'content' => $this->clips['__set'],
                        ),
-                       array('label' => Yii::t('P3AdminModule.crud', 'Models'),
+                       array('label'   => Yii::t('P3AdminModule.crud', 'Models'),
                              'content' => $this->clips['__md']),
 
                    )
