4acbdbb4b2a72ffb5514fee95389e32b080e32d0
Tobias Munk <schmunk@usrbin.de>
2013-07-19 09:43:39 +0200

    fixed old css classes

--- views/default/_user.php
+++ views/default/_user.php
@@ -1,41 +1,44 @@
+<h3>Information</h3>
 <div class="row">
-
-    <div class="span-4">
+    <div class="span4">
         <h4>Name</h4>
 
         <p><?php echo Yii::app()->user->name ?></p>
     </div>
-    <div class="span-4">
+    <div class="span4">
         <h4>Id</h4>
 
         <p><?php echo Yii::app()->user->id ?></p>
     </div>
 </div>
 
-<h3>Assigned Auth Items</h3>
+<h3>Assignments</h3>
 <div class="row">
     <div class="span4">
         <h4>Roles</h4>
 
         <p>
-            <?php foreach (Yii::app()->authManager->getAuthItems(2, Yii::app()->user->id) AS $key => $value)
-                echo $key . ", " ?>
+            <?php foreach (Yii::app()->authManager->getAuthItems(2, Yii::app()->user->id) AS $key => $value) {
+                echo $key . ", ";
+            } ?>
         </p>
     </div>
     <div class="span4">
         <h4>Operations</h4>
 
         <p><?php
-            foreach (Yii::app()->authManager->getAuthItems(1, Yii::app()->user->id) AS $key => $value)
-                echo $key . ", "
+            foreach (Yii::app()->authManager->getAuthItems(1, Yii::app()->user->id) AS $key => $value) {
+                echo $key . ", ";
+            }
             ?></p>
     </div>
     <div class="span4">
         <h4>Tasks</h4>
 
         <p><?php
-            foreach (Yii::app()->authManager->getAuthItems(0, Yii::app()->user->id) AS $key => $value)
-                echo $key . ", "
+            foreach (Yii::app()->authManager->getAuthItems(0, Yii::app()->user->id) AS $key => $value) {
+                echo $key . ", ";
+            }
             ?></p>
     </div>
 </div>
