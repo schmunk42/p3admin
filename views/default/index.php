ade1042d2221806a68969c958269c7716a94f5ca
Tobias Munk <schmunk@usrbin.de>
2013-10-02 01:54:26 +0200

    added customization info and README display

--- views/default/index.php
+++ views/default/index.php
@@ -10,8 +10,29 @@ $this->breadcrumbs = array(
     <small><?php echo Yii::t('P3AdminModule.crud', 'Dashboard'); ?></small>
 </h1>
 
-<?php $this->widget('p3widgets.components.P3WidgetContainer', array('id'                 => 'backend',
-                                                                    'varyByRequestParam' => P3Page::PAGE_ID_KEY,
-                                                                    'checkAccess'        => 'Admin')) ?>
+<div class="flash-notice">
+    Customize this view by overriding the module view in the backend theme.<br/>
+    <code>
+        mkdir -p app/themes/backend2/views/p3admin/default<br/>
+        cp vendor/phundament/p3admin/views/default/index.php app/themes/backend2/views/p3admin/default/index.php</code>
+</div>
+
+<?php
+$this->widget(
+    'p3widgets.components.P3WidgetContainer',
+    array(
+         'id'                 => 'backend',
+         'varyByRequestParam' => P3Page::PAGE_ID_KEY,
+         'checkAccess'        => 'Admin'
+    )
+) ?>
 
 
+<?php
+$this->beginWidget('CMarkdown');
+$readme = Yii::app()->basePath . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'README.md';
+if (is_file($readme)) {
+    echo file_get_contents($readme);
+}
+$this->endWidget();
+?>
